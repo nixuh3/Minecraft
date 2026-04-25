deffa82e221f8fb9e92867179647b91785155abb
nixuh3 <paulchuxin.liu@outlook.com>
2026-04-25 11:32:04 -0400

    Update ImGui, include spdlog as subdirectory

--- Voxel/src/Voxel/Application.cpp
+++ Voxel/src/Voxel/Application.cpp
@@ -52,12 +52,10 @@ void Application::OnEvent(Event& e) {
 
 void Application::PushLayer(Layer* layer) {
     m_LayerStack.PushLayer(layer);
-    layer->OnAttach();
 }
 
 void Application::PushOverlay(Layer* layer) {
     m_LayerStack.PushOverlay(layer);
-    layer->OnAttach();
 }
 
 bool Application::OnWindowClose(WindowCloseEvent& e) {
